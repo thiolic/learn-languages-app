047c750f417ea840e54253e5559bd94a976c05bd
Sanya MARYNYCH <sanyamarynych@o-s-i.org>
2023-10-27 17:36:10 +0200

    Added authentication

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+	return (
+		<main>
+			<h1 className="head-text">Onboarding</h1>
+		</main>
+	);
+};
+
+export default Page;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>Hello</h1>
-		</main>
-	);
-}
+const Home = () => {
+	return <h1 className="text-5xl">Hello</h1>;
+};
+
+export default Home;
